812fdebe6a460048b2c062b00fa34c5573ddbb73
Shin JIa <22521004@gm.uit.edu.vn>
2025-04-11 21:16:32 +0700

    update link to dashboard

--- middlewares/client/user.middleware.js
+++ middlewares/client/user.middleware.js
@@ -1,4 +1,5 @@
 const User = require("../../models/account.model");
+const Role = require("../../models/role.model");
 
 module.exports.infoUser = async (req, res, next) => {
     if (req.cookies.token) {
@@ -7,6 +8,10 @@ module.exports.infoUser = async (req, res, next) => {
             deleted: false,
         }).select("-password");
         if (user){
+            const role = await Role.findOne({
+                title: "admin"
+            })
+            res.locals.adminRole = role.id;
             req.user = user;
             res.locals.user = user;
         } 
